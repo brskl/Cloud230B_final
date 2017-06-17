581098246ccec52bf1a8bc8e327fcb17501e3ffb
brsklar <brskl@msn.com>
2017-06-16 17:39:00 -0700

    initial code, build up put parms

---
+++ trigCelebrityFaces.js
@@ -0,0 +1,48 @@
+'use strict';
+
+console.log('Loading function');
+var AWS = require("aws-sdk");
+var rekognition = new AWS.Rekognition();
+
+const RKG_COL = process.env.faceCollection;
+
+exports.handler = (event, context, callback) => {
+    putMatches(event);
+}
+
+function putMatches(searchData) {
+    console.log('Matches', searchData.FaceMatches.length);
+    var searchedFaceId = searchData.SearchedFaceId;
+    for (let faceMatch of searchData.FaceMatches) {
+        putMatch(searchedFaceId, faceMatch);
+    }
+}
+
+function putMatch(searchedFaceId, faceMatch) {
+    var faceId = faceMatch.Face.FaceId;
+    var similarity = faceMatch.Similarity;
+  
+    var params = {
+        TableName : 'CelebrityFaceMatches',
+        Item: {
+            MatchId: guid(),
+            FaceIdSearch: searchedFaceId,
+            FaceIdTarget: faceId,
+            Similarity: similarity
+        },
+        ConditionExpression: "attribute_not_exists(MatchId)",
+        ReturnConsumedCapacity: "TOTAL"
+    };
+  
+  console.log(params);
+  
+}
+
+function guid() {
+    function s4() {
+        return Math.floor((1 + Math.random()) * 0x10000)
+            .toString(16)
+            .substring(1);
+    }
+    return s4() + s4() + '-' + s4() + '-' + s4() + '-' + s4() + '-' + s4() + s4() + s4();
+}
