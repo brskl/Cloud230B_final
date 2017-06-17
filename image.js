8adc157f0a8d3a698a01e8f5daf7f58f4de21fbb
brsklar <brskl@msn.com>
2017-06-17 01:25:57 -0700

    added faces table and scan

--- image.js
+++ image.js
@@ -1,4 +1,5 @@
 var imageTemplate = Handlebars.compile( $('#imageTemplate').html());
+var facesTemplate = Handlebars.compile( $('#facesTemplate').html());
 
 function loadImagePage() {
   var fileid = getParameterByName('fileid');
@@ -19,11 +20,31 @@ function loadImagePage() {
         var tdata = { imagefile: data.Items[0] };
         var html = imageTemplate(tdata);
         $('#divImageTemplate').html(html);
+        scanFaces(fileid);
       }
     });
   } else {
     $('#divImageTemplate').html('Query parameter "fileid" not specified');
   }
+}
+
+function scanFaces(fileid) {
+  var params = {
+    TableName: "CelebrityFaces",
+    FilterExpression: "FileId = :fileidVal",
+    ExpressionAttributeValues: { ':fileidVal': fileid}
+  };
+  var dynamodbdoc = new AWS.DynamoDB.DocumentClient();
+  dynamodbdoc.scan(params, function(err, data) {
+    if (err) {
+      console.log(err, err.stack); // an error occurred
+      $('#divFacesTemplate').html('Unable to download data');
+    } else {
+      var tdata = { faces: data.Items };
+      var html = facesTemplate(tdata);
+      $('#divFacesTemplate').html(html);
+    }
+  });
 }
 
 function getParameterByName(name) {
