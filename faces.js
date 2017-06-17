1af96b2941ab3ba26f490c5a130f195ec169534e
brsklar <brskl@msn.com>
2017-06-17 13:28:10 -0700

    factorized code

--- faces.js
+++ faces.js
@@ -5,43 +5,51 @@ function loadFacesPage() {
   var faceid = getParameterByName('faceid');
   
   if (faceid) {
-    $('#divTableFaces').hide();
-    $('#divFace').show();
-    var dynamodbdoc = new AWS.DynamoDB.DocumentClient();
-    var params = {
-      TableName: "CelebrityFaces",
-      KeyConditionExpression: "FaceId = :faceidVal",
-      ExpressionAttributeValues: { ':faceidVal': faceid}
-    };
-    dynamodbdoc.query(params, function(err, data) {
-      if (err) {
-        console.log(err, err.stack); // an error occurred
-        $('#divFace').html('Unable to download data');
-      } else {
-        var tdata = { face: data.Items[0] };
-        var html = templateFace(tdata);
-        $('#divFace').html(html);
-      }
-    });
+    loadFace(faceid);
   } else {
-    $('#divTableFaces').show();
-    $('#divFace').hide();
-    var dynamodbdoc = new AWS.DynamoDB.DocumentClient();
-    var params = {
-      TableName: "CelebrityFaces",
-    };
-    dynamodbdoc.scan(params, function(err, data) {
-      if (err) {
-        console.log(err, err.stack); // an error occurred
-        $('#divTableFaces').html('Unable to download data');
-      } else {
-        var tdata = { faces: data.Items };
-        var html = templateTableFaces(tdata);
-        $('#divTableFaces').html(html);
-       
-      }
-    });
+    loadFaces();
   }
+}
+
+function loadFace(faceid) {
+  $('#divTableFaces').hide();
+  $('#divFace').show();
+  var dynamodbdoc = new AWS.DynamoDB.DocumentClient();
+  var params = {
+    TableName: "CelebrityFaces",
+    KeyConditionExpression: "FaceId = :faceidVal",
+    ExpressionAttributeValues: { ':faceidVal': faceid}
+  };
+  dynamodbdoc.query(params, function(err, data) {
+    if (err) {
+      console.log(err, err.stack); // an error occurred
+      $('#divFace').html('Unable to download data');
+    } else {
+      var tdata = { face: data.Items[0] };
+      var html = templateFace(tdata);
+      $('#divFace').html(html);
+      // TODO: show matches
+    }
+  });
+}
+
+function loadFaces() {
+  $('#divTableFaces').show();
+  $('#divFace').hide();
+  var dynamodbdoc = new AWS.DynamoDB.DocumentClient();
+  var params = {
+    TableName: "CelebrityFaces",
+  };
+  dynamodbdoc.scan(params, function(err, data) {
+    if (err) {
+      console.log(err, err.stack); // an error occurred
+      $('#divTableFaces').html('Unable to download data');
+    } else {
+      var tdata = { faces: data.Items };
+      var html = templateTableFaces(tdata);
+      $('#divTableFaces').html(html);
+    }
+  });
 }
 
 window.onload = loadFacesPage;
