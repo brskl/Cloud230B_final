14cff491e86349ec6599acdf096dba235d36aa1b
brsklar <brskl@msn.com>
2017-06-16 20:49:50 -0700

    fixed dynamodb create-table commands added some comments

--- projectInit.sh
+++ projectInit.sh
@@ -12,9 +12,9 @@ aws s3 cp <filename> s3://brsklar-celebrity-images
 aws s3api put-object-acl --acl public-read --bucket brsklar-celebrity-images --key <file> 
 
 // create tables
-aws dynamodb create-table --table-name CelebrityImageFiles --attribute-definitions AttributeName=FileId,AttributeType=S --key-schema AttributeName=FileId,KeyType=HASH --stream-specification --stream-specification StreamEnable=true,StreamViewType="NEW_AND_OLD_IMAGES" 
-aws dynamodb create-table --table-name CelebrityFaces --attribute-definitions AttributeName=FaceId,AttributeType=S --key-schema AttributeName=FaceId,KeyType=HASH --stream-specification StreamEnable=true,StreamViewType="NEW_AND_OLD_IMAGES"
-aws dynamodb create-table --table-name CelebrityFaceMatches --attribute-definitions AttributeName=FaceIdSearch,AttributeType=S AttributeName=FaceIdTarget,AttributeType=S --key-schema AttributeName=FaceId,KeyType=HASH AttributeName=FaceIdTarget,KeyType=RANGE
+aws dynamodb create-table --table-name CelebrityImageFiles --provisioned-throughput ReadCapacityUnits=5,WriteCapacityUnits=5 --attribute-definitions AttributeName=FileId,AttributeType=S --key-schema AttributeName=FileId,KeyType=HASH --stream-specification StreamEnabled=true,StreamViewType="NEW_AND_OLD_IMAGES" 
+aws dynamodb create-table --table-name CelebrityFaces --provisioned-throughput ReadCapacityUnits=5,WriteCapacityUnits=5 --attribute-definitions AttributeName=FaceId,AttributeType=S --key-schema AttributeName=FaceId,KeyType=HASH --stream-specification StreamEnabled=true,StreamViewType="NEW_AND_OLD_IMAGES"
+aws dynamodb create-table --table-name CelebrityFaceMatches --provisioned-throughput ReadCapacityUnits=5,WriteCapacityUnits=5  --attribute-definitions AttributeName=FaceIdSearch,AttributeType=S AttributeName=FaceIdTarget,AttributeType=S --key-schema AttributeName=FaceIdSearch,KeyType=HASH AttributeName=FaceIdTarget,KeyType=RANGE
 
 // create Rekognition collection
 aws rekognition create-collection --collection-id brsklar-celebrity-images
@@ -29,5 +29,7 @@ aws iam attach-role-policy --role-name Cognito_BrsklarCelebrityImagesUnauth_Role
 // create Cognito identity pool for unauth user access
 aws cognito-identity create-identity-pool --identity-pool-name BrsklarCelebrityImages --allow-unauthenticated-identities
 // set roles
-aws cognito-identity set-identity-pool-roles --identity-pool-id us-east-1:13515960-0a63-40df-bec5-ba1512f59d0e 
---roles <value>
+// ???
+
+// create lambda functions
+// attach triggers to lambda functions
