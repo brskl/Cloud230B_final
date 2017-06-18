1d5fd4c9ec817a1cd57d4c06d421f3e988b31bc6
brsklar <brskl@msn.com>
2017-06-17 17:02:42 -0700

    get individual match data and display

--- matches.js
+++ matches.js
@@ -34,9 +34,22 @@ function loadMatches() {
 }
 
 function loadMatch(faceids, faceidt) {
-  var tdata = { FaceIdSearch: faceids, FaceIdTarget: faceidt };
+  var dynamodbdoc = new AWS.DynamoDB.DocumentClient();
+  var params = {
+    TableName: "CelebrityFaceMatches",
+    FilterExpression: "FaceIdSearch = :faceidsVal AND FaceIdTarget = :faceidtVal",
+    ExpressionAttributeValues: { ':faceidsVal': faceids, ':faceidtVal': faceidt }
+  };
+  dynamodbdoc.scan(params, function(err, data) {
+    if (err) {
+      console.log(err, err.stack); // an error occurred
+      $('#divMatch').html('Unable to download data');
+    } else {
+      var tdata = { match: data.Items[0] };
       var html = templateMatch(tdata);
       $('#divMatch').html(html);
+    }
+  });
 }
 
 
