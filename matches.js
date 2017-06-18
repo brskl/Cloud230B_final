d0ec8afab71950938879bdb568c902dc92d89596
brsklar <brskl@msn.com>
2017-06-17 17:20:17 -0700

    get data from CelebrityFaces, added image

--- matches.js
+++ matches.js
@@ -60,10 +60,22 @@ function loadMatch(faceids, faceidt) {
 }
 
 function loadFace(div, title, faceid) {
-  var tdata = {Title: title, face: {FaceId : faceid}};
-        var html = templateFace(tdata);
+  var dynamodbdoc = new AWS.DynamoDB.DocumentClient();
+  var params = {
+    TableName: "CelebrityFaces",
+    KeyConditionExpression: "FaceId = :faceidVal",
+    ExpressionAttributeValues: { ':faceidVal': faceid}
+  };
+  dynamodbdoc.query(params, function(err, data) {
+    if (err) {
+      console.log(err, err.stack); // an error occurred
+      $(div).html('Unable to download data');
+    } else {
+      var tdata = {Title: title, face: data.Items[0]};
+      var html = templateFace(tdata);
       $(div).html(html);
-
+    }
+  });
 }
 
 
